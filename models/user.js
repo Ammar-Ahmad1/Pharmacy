8093b6daaa7d55d73120ee9a2f03a1500fc5b56a
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-08-30 09:59:15 +0500

    user register

---
+++ models/user.js
@@ -0,0 +1,33 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, 'Email already exists!'],
+    required: [true, 'Email is required!'],
+  },
+  name: {
+    type: String,
+    required: [true, 'name is required!'],
+  },
+  image: {
+    type: String,
+    default: 'https://lh3.googleusercontent.com/a/AAcHTtci6RJekFINCdibq0iNLKa9rfOSIFl04lh86s2ITxUu=s96-c'
+  },
+  password: {
+    type: String,
+  },
+  role: {
+    type: String,
+
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+
+
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,77 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import User from "../../../models/user";
+import { connectToDB } from "../../../config/database";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      authorize: async (credentials) => {
+        try {
+          await connectToDB();
+
+          const user = await User.findOne({
+            email: credentials.email,
+            password: credentials.password,
+          });
+          if (user) {
+            return {
+              id: user._id.toString(),
+              email: user.email,
+              name: user.name,
+              image: user.image,
+            };
+          } else {
+            throw new Error("Invalid email/password combination");
+          }
+        } catch (error) {
+          console.log("Error logging in user: ", error.message);
+          return null;
+        }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        
+        await connectToDB();
+
+        if (account.provider === "google") {
+          const userExists = await User.findOne({ email: profile.email });
+
+          if (!userExists) {
+            await User.create({
+              email: profile.email,
+              name: profile.name,
+              image: profile.picture,
+            });
+          }
+        } else if (account.provider === "credentials") {
+          // Handle credentials logic if needed, otherwise leave it as it's already handled in the provider
+        }
+
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
+  },
+});
+
+export default handler;
