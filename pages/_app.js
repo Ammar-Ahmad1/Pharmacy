b90864e6bf6a4418099c8878d79f445fac74b63d
Ammar-Ahmad1 <i190678@nu.edu.pk>
2023-11-01 11:22:14 +0500

    web analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 // import "react-input-range/lib/css/index.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import { Provider as ReduxProvider } from "react-redux";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import { SessionProvider } from "next-auth/react";
-import Provider from '../components/Provider'
-import 'react-toastify/dist/ReactToastify.css';
+import Provider from "../components/Provider";
+import "react-toastify/dist/ReactToastify.css";
 // import "slick-carousel/slick/slick-theme.css";
 // import "slick-carousel/slick/slick.css";
 import "react-responsive-modal/styles.css";
@@ -17,57 +17,63 @@ import StorageWrapper from "../components/ecommerce/storage-wrapper";
 import "../public/assets/css/main.css";
 import store from "../redux/store";
 import Preloader from "./../components/elements/Preloader";
-
+import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
+  // Preloader useState + Function
 
-    // Preloader useState + Function
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
 
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 1000);
+    // new WOW.WOW({
+    //     live: false
+    //   }).init()
+  }, []);
 
-        // new WOW.WOW({
-        //     live: false
-        //   }).init()
-    }, []);
+  return (
+    <>
+      {!loading ? (
+        <ReduxProvider store={store}>
+          <StorageWrapper>
+            <Provider>
+              <Head>
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1"
+                />
+                <link
+                  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+                  rel="stylesheet"
+                  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+                  crossOrigin="anonymous"
+                />
+              </Head>
+              <Script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+                crossOrigin="anonymous"
+              />
 
-
-    return (
-
-        <>
-            {!loading ? (
-                <ReduxProvider store={store}>
-                    <StorageWrapper>
-                        <Provider>
-
-                            <Head>
-                                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
-                            </Head>
-                            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous" />
-
-                            <Component {...pageProps} />
-                            <ToastContainer 
-                        autoClose={1000}
-                        />
-                        </Provider>
-                    </StorageWrapper>
-                </ReduxProvider>
-            ) : (
-                <Preloader />
-            )}
-        </>
-    );
+              <Component {...pageProps} />
+              <ToastContainer autoClose={1000} />
+              <Analytics />
+            </Provider>
+          </StorageWrapper>
+        </ReduxProvider>
+      ) : (
+        <Preloader />
+      )}
+    </>
+  );
 }
 
 export default MyApp;
-
 
 // Page with image preloader
 
